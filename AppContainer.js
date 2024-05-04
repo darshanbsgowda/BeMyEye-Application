393319198f97294b446888fc0e5ca6a687ed8c7a
darshanbsgowda <darshanbsgowda2003@gmail.com>
2024-05-04 11:13:54 +0530

    Add files via upload

--- AppContainer.js
+++ AppContainer.js
@@ -1,12 +1,55 @@
-// NOTE(EvanBacon): `react-native-web` adds a extra `div`s around the root HTML, these
-// make static rendering much harder as we expect the root element to be `<html>`.
-// This resolution will alias to a simple in-out component to avoid the extra HTML.
-function AppContainer({ children }) {
-  return children;
-}
+/**
+ * Copyright (c) Nicolas Gallagher.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
 
-if (process.env.NODE_ENV !== 'production') {
-  AppContainer.displayName = 'AppContainer';
-}
+import * as React from 'react';
+import StyleSheet from '../StyleSheet';
+import View from '../View';
+
+type Props = {
+  WrapperComponent?: ?React.ComponentType<*>,
+  // $FlowFixMe
+  children?: React.Children,
+  rootTag: any
+};
+
+const RootTagContext: React.Context<any> = React.createContext(null);
+
+const AppContainer: React.AbstractComponent<Props> = React.forwardRef(
+  (props: Props, forwardedRef?: React.Ref<any>) => {
+    const { children, WrapperComponent } = props;
+
+    let innerView = (
+      <View children={children} key={1} style={styles.appContainer} />
+    );
+
+    if (WrapperComponent) {
+      innerView = <WrapperComponent>{innerView}</WrapperComponent>;
+    }
+
+    return (
+      <RootTagContext.Provider value={props.rootTag}>
+        <View ref={forwardedRef} style={styles.appContainer}>
+          {innerView}
+        </View>
+      </RootTagContext.Provider>
+    );
+  }
+);
+
+AppContainer.displayName = 'AppContainer';
 
 export default AppContainer;
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    pointerEvents: 'box-none'
+  }
+});
